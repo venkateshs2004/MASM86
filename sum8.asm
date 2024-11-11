c83b7499a877886af8e28a2e506116480b039886
Venkatesh S <92909486+venkateshs2004@users.noreply.github.com>
2024-11-12 00:10:04 +0530

    Add files via upload

--- sum8.asm
+++ sum8.asm
@@ -3,36 +3,54 @@ ASSUME CS:CODE, DS:DATA
 DATA SEGMENT
     n1 DB 0AH,0DH,"ENTER NUMBER 1:$"
     n2 DB 0AH,0DH,"ENTER NUMBER 2:$"
-    MSG2 DB 10, 13, "OUTPUT:$"
+    MSG2 DB 0AH,0DH,"OUTPUT:$"
+  NUMBUFFER DB 3 DUP('$')
 DATA ENDS
 
 CODE SEGMENT
 START:
     MOV AX, DATA
     MOV DS, AX
-    LEA DX, n1
-    MOV AH, 09H
+
+    LEA DX,n1 ;1ST DIGIT
+    MOV AH,09H
     INT 21H
     MOV AH,01H
     INT 21H
-    mov bl,al
-    LEA DX, n2
-    MOV AH, 09H
+    CMP AL,'A'
+    JB DIGIT1
+    SUB AL,07H
+DIGIT1:SUB AL,30H
+    MOV BL, AL
+
+    LEA DX,n2 ;2ND DIGIT
+    MOV AH,09H
     INT 21H
     MOV AH,01H
-	INT 21H
-    ADD al,bl
-    ADC BH, 30H
-    ADD al,30H
-    LEA DX, MSG2
-    MOV AH, 09H
-    INT 21H   
-    MOV DL, BH
-    MOV AH, 02H
     INT 21H
-    MOV DL, AL
-    MOV AH, 02H
-    INT 21H         
+    CMP AL,'A'
+    JB DIGIT2
+    SUB AL,07H
+DIGIT2:SUB AL,30H
+
+    ; Add the two numbers
+    ADD AL, BL
+    MOV CL, 10 
+
+    LEA DX,MSG2
+    MOV AH,09H
+    INT 21H
+DIGIT:XOR AH, AH
+    DIV CL
+    ADD AL,'0'
+    MOV [NUMBUFFER],AL
+    MOV DL,AH
+    ADD DL,'0'
+    MOV [NUMBUFFER+1],DL
+    LEA DX,NUMBUFFER
+    MOV AH,09H
+    INT 21H  
+    ; Exit program
     MOV AH, 4CH
     INT 21H
 CODE ENDS
